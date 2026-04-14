5a65916154b892fba69244711e46dd96b9c47828
Daniel loy braggs <117001844+daniel-loy@users.noreply.github.com>
2026-04-14 22:12:02 +0530

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount+1];
+        dp[0]=1;
+        for(int coin : coins){
+
+    for(int i=1;i<amount+1;i++){
+            if(coin<=i && dp[i-coin]>0){
+                dp[i]+=dp[i-coin];
+            }
+        }
+    }
+
+    return dp[amount];
+
+    }
+}
