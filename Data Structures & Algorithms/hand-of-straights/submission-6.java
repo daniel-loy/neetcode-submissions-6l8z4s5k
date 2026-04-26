b13e1a4b6696d6a1036578f292fcd57ced1243c6
Daniel loy braggs <117001844+daniel-loy@users.noreply.github.com>
2026-04-26 16:37:43 +0530

    Add: hand-of-straights - submission-6

---
+++ Data Structures & Algorithms/hand-of-straights/submission-6.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        Arrays.sort(hand);
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int num : hand){
+            int value = map.getOrDefault(num,0);
+            map.put(num,value+1);
+        }
+
+        for(int i=0;i<hand.length;i++){
+            int groupno = map.get(hand[i]);
+            if(groupno==0){
+                continue;
+            }
+            
+            for(int j=0;j<groupSize;j++){
+                int currenthand = hand[i]+j;
+
+                if(map.containsKey(currenthand) && map.get(currenthand)>=groupno){
+                    map.put(currenthand,map.get(currenthand)-1);
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
