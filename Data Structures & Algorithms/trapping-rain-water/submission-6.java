d6d1196bfcdcd404d766aa4abb128bc8645f9ea9
Daniel loy braggs <117001844+daniel-loy@users.noreply.github.com>
2026-09-05 15:16:45 +0530

    Add: trapping-rain-water - submission-6

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-6.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int trap(int[] height) {
+        int maxwater = 0;
+        int left =0;
+        int right = height.length-1;
+        int leftmax = height[left];
+        int rightmax = height[right];
+
+        while(left<right)
+        {
+		if(height[left]<height[right]){
+                if(leftmax<height[left])
+                {
+                    leftmax = height[left];
+                }
+                else{
+                    maxwater+=leftmax-height[left];
+                                    left++;
+
+                }
+                //System.out.println("maxwater "+maxwater+" left index :"+left+" value :"+height[left]);
+            }
+            else{
+                if(rightmax<height[right])
+                {
+                    rightmax = height[right];
+                }
+                else{
+                    maxwater+=rightmax-height[right];
+                                    right--; 
+
+                }
+                    //System.out.println("maxwater "+maxwater+" right index :"+right+" value :"+height[right]);
+
+            }
+        }
+        return maxwater;
+    }
+}
